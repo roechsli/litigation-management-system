a89bc27ccd33270cce0371090f71e3a1008d6011
Roman Oechslin <romanoech@gmail.com>
2023-09-16 17:05:01 +0200

    split fields on two rows

--- lib/screens/claim_view.dart
+++ lib/screens/claim_view.dart
@@ -58,110 +58,132 @@ class _EditClaimScreenState extends State<EditClaimScreen> {
     "volume"
   ];
 
+  List<Widget> _getListOfTextFields() {
+    return fieldNameMapping.keys.map((key) {
+      final value = fieldNameMapping[key];
+      return _InfoRow(
+          title: key,
+          value: claimData[value],
+          isProvided: providedFields.contains(value),
+          editWidget: _buildEditableField(
+              value, claimData[value], providedFields.contains(value)),
+          editMode: editMode);
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> textFields = [];
+    textFields.addAll(_getListOfTextFields());
+
+    final int halfLength = (textFields.length / 2).ceil();
+
     return SingleChildScrollView(
-      child: Column(
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: editMode
-                      ? null
-                      : () {
-                    setState(() {
-                      editMode = !editMode;
-                    });
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.save),
-                  onPressed: editMode
-                      ? () {
-                    // Save changes and exit edit mode
-                    setState(() {
-                      editMode = false;
-                    });
-                  }
-                      : null, // Set onPressed to null when editMode is false
-                ),
-                IconButton(
-                  icon: Icon(Icons.cancel),
-                  onPressed: editMode
-                      ? () {
-                    // Cancel changes and exit edit mode
-                    setState(() {
-                      editMode = false;
-                    });
-                  }
-                      : null,
-                ),
-              ],
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: editMode
+                  ? null
+                  : () {
+                      setState(() {
+                        editMode = !editMode;
+                      });
+                    },
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: editMode
+                  ? () {
+                      // Save changes and exit edit mode
+                      setState(() {
+                        editMode = false;
+                      });
+                    }
+                  : null, // Set onPressed to null when editMode is false
+            ),
+            IconButton(
+              icon: Icon(Icons.cancel),
+              onPressed: editMode
+                  ? () {
+                      // Cancel changes and exit edit mode
+                      setState(() {
+                        editMode = false;
+                      });
+                    }
+                  : null,
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                          child: _MoneyBox(
-                            icon: Icons.attach_money,
-                            title: "Total",
-                            value: claimData['volume'],
-                            currency: claimData['currency'],
-                            color: Colors.indigo,
-                          )),
-                      SizedBox(width: 10),
-                      Expanded(
-                          child: _MoneyBox(
-                            icon: Icons.money_off,
-                            title: "Paid",
-                            value: "1000",
-                            currency: claimData['currency'],
-                            color: Colors.green,
-                          )), // Replace with actual paid amount
-                      SizedBox(width: 10),
-                      Expanded(
-                          child: _MoneyBox(
-                            icon: Icons.payment,
-                            title: "Due",
-                            value: "2000",
-                            currency: claimData['currency'],
-                            color: Colors.deepOrangeAccent,
-                          )), // Replace with actual due amount
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  _SectionTitle(title: "General Info"),
-                  SizedBox(height: 10),
-                  ...fieldNameMapping.keys.map((key) {
-                    final value = fieldNameMapping[key];
-                    return editMode
-                        ? _buildEditableField(value, claimData[value])
-                        : _InfoRow(
-                      title: key,
-                      value: claimData[value],
-                      isProvided: providedFields.contains(value),
-                    );
-                  }).toList(),
+                  Expanded(
+                      child: _MoneyBox(
+                    icon: Icons.attach_money,
+                    title: "Total",
+                    value: claimData['volume'],
+                    currency: claimData['currency'],
+                    color: Colors.indigo,
+                  )),
+                  SizedBox(width: 10),
+                  Expanded(
+                      child: _MoneyBox(
+                    icon: Icons.money_off,
+                    title: "Paid",
+                    value: "1000",
+                    currency: claimData['currency'],
+                    color: Colors.green,
+                  )), // Replace with actual paid amount
+                  SizedBox(width: 10),
+                  Expanded(
+                      child: _MoneyBox(
+                    icon: Icons.payment,
+                    title: "Due",
+                    value: "2000",
+                    currency: claimData['currency'],
+                    color: Colors.deepOrangeAccent,
+                  )), // Replace with actual due amount
                 ],
               ),
-            ),]
-      ),
+              SizedBox(height: 20),
+              _SectionTitle(title: "General Info"),
+              SizedBox(height: 10),
+
+             Row(
+               mainAxisAlignment: MainAxisAlignment.start,
+               children: [
+                 Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: textFields.sublist(0, halfLength),
+                 ),
+                 Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: textFields.sublist(halfLength),
+                 ),
+               ],
+             ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 
-  Widget _buildEditableField(String field, String initialValue) {
+  Widget _buildEditableField(
+      String field, String initialValue, bool isProvided) {
     return TextFormField(
       initialValue: initialValue,
       decoration: InputDecoration(
         border: OutlineInputBorder(),
       ),
-      style: TextStyle(color: Colors.grey),
+      style: TextStyle(color: isProvided ? Colors.black : Colors.grey),
       onChanged: (newValue) {
         // Update the claimData map with the new value
         setState(() {
@@ -229,24 +251,40 @@ class _InfoRow extends StatelessWidget {
   final String title;
   final String value;
   final bool isProvided;
+  final bool editMode;
+  final Widget editWidget;
 
   _InfoRow(
-      {required this.title, required this.value, required this.isProvided});
+      {required this.title,
+      required this.value,
+      required this.isProvided,
+      required this.editWidget,
+      required this.editMode});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 2,
-          child: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-        ),
-        Expanded(
-          flex: 2,
-          child: Text(value,
-              style: TextStyle(color: isProvided ? Colors.black : Colors.grey)),
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.all(4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(
+            width: 20,
+          ),
+          Container(
+            width: 200,
+            child: editMode
+                ? editWidget
+                : Padding(
+                    padding: EdgeInsets.all(18),
+                    child: Text(value,
+                        style: TextStyle(
+                            color: isProvided ? Colors.black : Colors.grey)),
+                  ),
+          )
+        ],
+      ),
     );
   }
 }
